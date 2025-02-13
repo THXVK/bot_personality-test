5c6a567e89aefe880f5db85e0c00eb47b1217cd7
Артём Сергеевич <artemsavelev356@gmail.com>
2025-02-14 01:01:22 +0300

    небольшое обновление основного цикла, больше тасок

--- code/bot.py
+++ code/bot.py
@@ -6,18 +6,19 @@ from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
-from data import q_data, users, questions
+from data import users, questions
 from keyboards import answers_kb
 
 router = Router()
-
+q_limit = 30
 
 #  todo: выбор сложности
 
 @router.message(CommandStart())
 async def start_message(message: Message):
     await message.reply('Привет!\nЭто бот-тест на тип личности, основанный на MBTI\n'
-                        'Чтобы начать тест, напиши /start_test')
+                        'Чтобы начать тест, напиши /start_test')# todo: проверка на рестарт
+    #todo: выбор сложности на {q_limit}
     print(message.user_id)
     print(message.chat.id)
 
@@ -29,7 +30,8 @@ async def send_commands_list(message: Message):
 
 @router.message(Command('start_test'))
 async def send_first_question_text(message: Message):
-    await message.reply(f'вопрос 1\n{q_data[1]}', reply_markup=answers_kb)
+    await message.reply(f'Вопрос 1/{q_limit}\n{questions[1]['text']}', reply_markup=answers_kb)
+
 
 @router.message(Command('continue'))
 async def continue_test(message: Message):
@@ -47,13 +49,23 @@ async def send_confirmation_text(message: Message):
     if message.text in ['1', '2', '3', '4', '5']:
         answer = -1 + 0.5 * (int(message.text) - 1)
         users[user_id][axe][answer] += answer
-        ...
+        await message.reply('Принято!')
+        users[user_id]['q_num'] += 1
+        if q_num := users[user_id]['q_num'] > q_limit:
+            pass  # todo: финал теста
+
+    elif message.text.startswith('/'):
+        await message.reply('Для вызова команд напишите /stop')
+        await send_next_question(message, q_num)
     elif message.text == '/stop_test':
-        await message.reply('вы остановили тест. Для его продолжения напишите /continue',
+        await message.reply('Вы остановили тест. Для его продолжения напишите /continue',
                             reply_markup=ReplyKeyboardRemove())
     else:
-        await message.reply('неверный ответ')
-        ...
+        await message.reply('Неверный ответ')
+        await send_next_question(message, q_num)
 
 
 async def send_next_question(message, q_number):
+    text = questions[q_number]['text']
+    await message.reply(f'Вопрос {q_number}/{q_limit}\n{text}')
+    # todo: FSM states?
