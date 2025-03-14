d124573e286fdf68f8f7c7d0f939916c21c832f7
Артём Сергеевич <artemsavelev356@gmail.com>
2025-03-14 23:46:21 +0300

    все python словари перенесены в config.py

--- code/data.py
+++ code/data.py
@@ -1,4 +1,3 @@
-
 users = {
     12314442: {
         'q_num': 1,
@@ -12,140 +11,69 @@ users = {
     }
 }
 
-questions = {
-    1: {
-        'text': 'составляю списки | полагаюсь на память',
-        'axe': 'J - P'
-    },
-    2: {
-        'text': 'настроен скептически | хочу верить',
-        'axe': 'F - T'
-    },
-    3: {
-        'text': 'нуждаюсь в одиночестве | скучаю в нем',
-        'axe': 'I - E'
-    },
-    4: {
-        'text': 'принимаю все как есть | недоволен положением вещей',
-        'axe': 'F - T'
-    },
-    5: {
-        'text': 'порядок в комнате | разбрасываю вещи',
-        'axe': 'J - P'
-    },
-    6: {
-        'text': 'сравнение с роботом для меня обидно | стремлюсь мыслить как механизм',
-        'axe': 'F - T'
-    },
-    7: {
-        'text': 'мягкий | энергичный',
-        'axe': 'I - E'
-    },
-    8: {
-        'text': 'написать эссе | пройти тест',
-        'axe': 'J - P'
-    },
-    9: {
-        'text': 'хаотичный | организованный',
-        'axe': 'J - P'
-    },
-    10: {
-        'text': 'легкоранимый | толстокожий',
-        'axe': 'F - T'
-    },
-    11: {
-        'text': 'работа самостоятельно | работа в коллективе',
-        'axe': 'I - E'
-    },
-    12: {
-        'text': 'фокус на будущем | фокус на настоящем',
-        'axe': 'S - N'
-    },
-    13: {
-        'text': 'планирую наперед | решаю в последнюю минуту',
-        'axe': 'J - P'
-    },
-    14: {
-        'text': 'меня уважают | меня любят',
-        'axe': 'F - T'
-    },
-    15: {
-        'text': 'вечеринки утомляют | заряжаюсь бодростью на них',
-        'axe': 'I - E'
-    },
-    16: {
-        'text': 'вписываюсь в коллектив | держусь особняком',
-        'axe': 'I - E'
-    },
-    17: {
-        'text': 'оставляю себе свободу выбора | беру обязательства на себя',
-        'axe': 'J - P'
-    },
-    18: {
-        'text': 'хочу чинить вещи | хочу исправлять людей',
-        'axe': 'F - T'
-    },
-    19: {
-        'text': 'говорю | слушаю',
-        'axe': 'I - E'
-    },
-    20: {
-        'text': 'опишу событие | перескажу его смысл',
-        'axe': 'S - N'
-    },
-    21: {
-        'text': 'работаю сразу | откладываю на потом',
-        'axe': 'J - P'
-    },
-    22: {
-        'text': 'слушаю сердце | слушаю рассудок',
-        'axe': 'F - T'
-    },
-    23: {
-        'text': 'сижу дома | провожу время вне дома',
-        'axe': 'I - E'
-    },
-    24: {
-        'text': 'видеть общую картину | видеть детали',
-        'axe': 'S - N'
-    },
-    25: {
-        'text': 'импровизирую | готовлюсь',
-        'axe': 'J - P'
-    },
-    26: {
-        'text': 'справедливость | сострадание',
-        'axe': 'F - T'
-    },
-    27: {
-        'text': 'не люблю кричать | могу крикнуть кому-то на расстоянии',
-        'axe': 'I - E'
-    },
-    28: {
-        'text': 'теоретик | практик',
-        'axe': 'S - N'
-    },
-    29: {
-        'text': 'делаю дело | гуляю смело',
-        'axe': 'I - E'
-    },
-    30: {
-        'text': 'дискомфорт от эмоций | ценю их',
-        'axe': 'F - T'
-    },
-    31: {
-        'text': 'люблю выступать | избегаю публики',
-        'axe': 'I - E'
-    },
-    32: {
-        'text': 'Что? Где? Когда? | Почему?',
-        'axe': 'S - N'
-    },
-}
+import asyncio
+from sqlalchemy import create_engine, BigInteger, select
+from sqlalchemy.orm import registry, declarative_base, as_declarative, mapped_column, Mapped, Session, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs, async_sessionmaker, AsyncSession
 
-base_letters = {
-            'I - E': 'I',
-            'S - N': 'N',
-            'F - T': 'T',
-            'J - P': 'J',
-}
+engine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=True)
+async_s = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
+
+class UsersTable(Base):
+    __tablename__ = 'users'
+    user_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    question_num: Mapped[int] = mapped_column()
+    I_E: Mapped[float] = mapped_column()
+    S_N: Mapped[float] = mapped_column()
+    F_T: Mapped[float] = mapped_column()
+    J_P: Mapped[float] = mapped_column()
+    p_type: Mapped[str] = mapped_column()
+
+
+async def create_table():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def add_user(async_session: async_sessionmaker[AsyncSession], user_id):
+    async with async_session() as session:
+        async with session.begin():
+            user = UsersTable(user_id=user_id, question_num=1, I_E=0, S_N=0, F_T=0, J_P=0, p_type='untitled')
+            session.add(user)
+
+
+# todo: функция обновления данных
+
+
+async def get_data(async_session: async_sessionmaker[AsyncSession], user_id):
+    async with async_session() as session:
+        async with session.begin():
+            res = await session.execute(select(UsersTable).where(UsersTable.user_id == user_id))
+            user = res.scalar_one_or_none()
+            if user:
+                return {
+                    "user_id": user.user_id,
+                    "I_E": user.I_E,
+                    "S_N": user.S_N,
+                    "F_T": user.F_T,
+                    "J_P": user.J_P,
+                    "p_type": user.p_type,
+                }
+            return None
+
+
+async def main():
+    """Основная асинхронная функция для тестирования."""
+    await create_table()
+    await add_user(async_s, 12333)
+    user = await get_data(async_s, 12333)
+    print(user)
+
+
+# Запуск асинхронного кода
+asyncio.run(main())
