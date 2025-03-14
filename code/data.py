6330667d6c3a57c21213a1ae7f08c881c40b4aa5
Артём Сергеевич <artemsavelev356@gmail.com>
2025-03-15 02:57:13 +0300

    новая функция update_user_data

--- code/data.py
+++ code/data.py
@@ -1,23 +1,9 @@
-users = {
-    12314442: {
-        'q_num': 1,
-        'axes': {
-            'I - E': 0,
-            'S - N': 0,
-            'F - T': 0,
-            'J - P': 0,
-        },
-        'p_type': 'untitled'
-    }
-}
-
-
 import asyncio
-from sqlalchemy import create_engine, BigInteger, select
-from sqlalchemy.orm import registry, declarative_base, as_declarative, mapped_column, Mapped, Session, DeclarativeBase
+from sqlalchemy import BigInteger, select
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs, async_sessionmaker, AsyncSession
 
-engine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=True)
+engine = create_async_engine('sqlite+aiosqlite:///:memory:')
 async_s = async_sessionmaker(engine, expire_on_commit=False)
 
 
@@ -28,7 +14,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 class UsersTable(Base):
     __tablename__ = 'users'
     user_id: Mapped[int] = mapped_column(BigInteger, unique=True)
-    question_num: Mapped[int] = mapped_column()
+    q_num: Mapped[int] = mapped_column()
     I_E: Mapped[float] = mapped_column()
     S_N: Mapped[float] = mapped_column()
     F_T: Mapped[float] = mapped_column()
@@ -44,11 +30,20 @@ async def create_table():
 async def add_user(async_session: async_sessionmaker[AsyncSession], user_id):
     async with async_session() as session:
         async with session.begin():
-            user = UsersTable(user_id=user_id, question_num=1, I_E=0, S_N=0, F_T=0, J_P=0, p_type='untitled')
-            session.add(user)
+            if not await get_data(async_session, user_id):
+                user = UsersTable(user_id=user_id, q_num=1, I_E=0, S_N=0, F_T=0, J_P=0, p_type='untitled')
+                session.add(user)
+                return True
+            return None
 
 
-# todo: функция обновления данных
+async def update_user_data(async_session: async_sessionmaker[AsyncSession], user_id, column, new_data) -> None:
+    async with async_session() as session:
+        async with session.begin():
+            res = await session.execute(select(UsersTable).where(UsersTable.user_id == user_id))
+            user = res.scalar_one_or_none()
+            if hasattr(user, column):
+                setattr(user, column, new_data)
 
 
 async def get_data(async_session: async_sessionmaker[AsyncSession], user_id):
@@ -59,10 +54,13 @@ async def get_data(async_session: async_sessionmaker[AsyncSession], user_id):
             if user:
                 return {
                     "user_id": user.user_id,
-                    "I_E": user.I_E,
-                    "S_N": user.S_N,
-                    "F_T": user.F_T,
-                    "J_P": user.J_P,
+                    'question_num': user.q_num,
+                    'axes': {
+                        "I_E": user.I_E,
+                        "S_N": user.S_N,
+                        "F_T": user.F_T,
+                        "J_P": user.J_P,
+                    },
                     "p_type": user.p_type,
                 }
             return None
@@ -71,7 +69,11 @@ async def get_data(async_session: async_sessionmaker[AsyncSession], user_id):
 async def main():
     """Основная асинхронная функция для тестирования."""
     await create_table()
+
     await add_user(async_s, 12333)
+
+    await update_user_data(async_s, 12333, 'q_num', 2)
+
     user = await get_data(async_s, 12333)
     print(user)
 
